4a630302ff0a39ddf7b8b297f400cd40081f16f8
Hamza Ahmed <kojozcodes@gmail.com>
2025-03-25 21:53:59 +0100

    git complete kanye_quote project and add issoverhead starting files

--- day-33/main.py
+++ day-33/main.py
@@ -1,10 +1,31 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 9.076479
+MY_LONG = 7.398574
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position_tuple = (longitude, latitude)
+# print(iss_position_tuple)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position_tuple = (longitude, latitude)
-print(iss_position_tuple)
+sunrise = str(int(data["results"]["sunrise"].split("T")[1].split(":")[0]) + 1)
+sunset = str(int(data["results"]["sunset"].split("T")[1].split(":")[0]) + 1)
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
